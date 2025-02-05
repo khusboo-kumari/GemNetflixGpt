03ade1bb52201f6948e330ef10e17235b65d84f5
khusboo-kumari <dyttokhus@gmail.com>
2025-02-05 11:50:01 +0530

    password msg

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,15 +1,15 @@
-export const checkValidData = (email, password, name)  => {
+export const checkValidData = (email, password, name) => {
 
-    const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email) ; 
+    const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
 
-   const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password) ;
-   
-   const isValidName = /([a-zA-Z0-9_\s]+)/.test(name) ; 
-    
-    if(!isEmailValid) return "Email Id is not valid" ; 
-    if(!isPasswordValid) return "Password is not valid" ; 
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
-    if(!isValidName) return "Improper name " ; 
+    const isValidName = /([a-zA-Z0-9_\s]+)/.test(name);
 
-   return null ; // null means everything is right 
+    if (!isEmailValid) return "Email Id is not valid";
+    if (!isPasswordValid) return "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, a special character and one number.";
+
+    if (!isValidName) return "Improper name ";
+
+    return null; // null means everything is right 
 }
